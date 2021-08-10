6251d51f5f89ddeef301142d45942497c2d3a47f
Ckiv <baltazar1995>
2021-08-10 23:50:14 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(typeof array != "undefined" && array != null && array.length > 0){
+        let result=array[0];
+        array.forEach(function(item, index, array) {
+            if (item<result) {
+                result=item;
+            }
+        });
+        return result;
+    }
+    else {return 0}
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(typeof array != "undefined" && array != null && array.length > 0){
+        let result=array[0];
+        array.forEach(function(item, index, array) {
+            if (item>result) {
+                result=item;
+            }
+        });
+        return result;
+    }
+    else {return 0}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(typeof array != "undefined" && array != null && array.length > 0){
+        let result=0;
+        array.forEach(function(item, index, array) {
+                result+=item;
+        });
+        return result/array.length;
+        }
+    else {return 0}
 }
